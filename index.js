83bcf702637a8be277f9e4e3a0e331c29609effa
RohanK22 <rohank2205@gmail.com>
2021-05-25 14:22:20 +0530

    Done for the day

--- index.js
+++ index.js
@@ -8,6 +8,15 @@ let app = express();
 app.use(express.static('public'));
 app.use(express.json());
 
+app.get('/api/codeforces/upcoming-contests', (req, res) => {
+	axios.get('https://codeforces.com/contests').then((response) => {
+		const $ = cheerio.load(response);
+		let dataTableDiv = $('.datatable tr').text();
+		console.log(dataTableDiv);
+		res.send(dataTableDiv);
+	});
+});
+
 app.listen(3000, () => {
 	console.log('listening on port 3000');
 });
--- public/index.js
+++ public/index.js
@@ -1,10 +1,12 @@
 const apiBaseUrl = 'http://localhost:3000/api/';
 const username = 'rohank2205';
+
+const codeforcesEle = document.getElementById('codeforces');
+
 // Set user info
 fetch('https://codeforces.com/api/user.rating?handle=' + username)
 	.then((response) => response.json())
 	.then((data) => {
-		const codeforcesEle = document.getElementById('codeforces');
 		const displayHandleEle = document.createElement('h2');
 		const currentRatingEle = document.createElement('h3');
 		const contestDisplay = document.createElement('div');
@@ -14,19 +16,31 @@ fetch('https://codeforces.com/api/user.rating?handle=' + username)
 			'Current User Rating: ' +
 			data.result[data.result.length - 1].newRating;
 		data.result = data.result.reverse();
-		console.log(data.result);
 		for (let contest = 0; contest < data.result.length; contest++) {
 			let pTag = document.createElement('p');
 			pTag.textContent =
-				contest + 1 + '. ' + data.result[contest].contestName;
+				contest +
+				1 +
+				'. ' +
+				data.result[contest].contestName +
+				'      Rank: ' +
+				data.result[contest].rank;
 			contestDisplay.appendChild(pTag);
 		}
 
 		// Add all children
 		codeforcesEle.appendChild(displayHandleEle);
 		codeforcesEle.appendChild(currentRatingEle);
+		const tTag = document.createElement('h3');
+		tTag.textContent = 'Contests Attended';
+		codeforcesEle.append(tTag);
 		codeforcesEle.appendChild(contestDisplay);
 	})
 	.catch((error) => {
 		console.log('Error Occoured: ' + error);
 	});
+
+// Set Upcoming cosntests list with time left in hrs
+fetch(apiBaseUrl + 'codeforces/upcoming-contests').then((data) =>
+	console.log(data)
+);
