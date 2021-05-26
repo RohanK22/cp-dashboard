506ded0dc9547b81c5713dd6f49af7be58cb5e9a
RohanK22 <rohank2205@gmail.com>
2021-05-26 20:07:35 +0530

    Finished

--- index.js
+++ index.js
@@ -1,20 +1,114 @@
 const cheerio = require('cheerio');
 const express = require('express');
-const axios = require('axios');
-
-const username = 'rohank2205';
+const request = require('request-promise');
+const fetch = require('node-fetch');
 
 let app = express();
 app.use(express.static('public'));
 app.use(express.json());
 
-app.get('/api/codeforces/upcoming-contests', (req, res) => {
-	axios.get('https://codeforces.com/contests').then((response) => {
-		const $ = cheerio.load(response);
-		let dataTableDiv = $('.datatable tr').text();
-		console.log(dataTableDiv);
-		res.send(dataTableDiv);
+// Scrape stuff from codeforces.com and return the contests found
+app.get('/api/codeforces/upcoming-contests', async (req, res) => {
+	let arrayOfContests = [];
+	const response = await request.get('https://codeforces.com/contests');
+	let $ = cheerio.load(response);
+	$(
+		'#pageContent > div.contestList > div.datatable > div:nth-child(6) > table > tbody > tr'
+	).each((index, element) => {
+		if (index === 0) return true; // Ignore the first row
+		let contest = {};
+		$('td', element).each((index, c) => {
+			if (index === 0) {
+				contest.name = $(c).text().replace(/\s+/g, ' ').trim();
+			} else if (index === 2) {
+				contest.dateTime = $(c).text().replace(/\s+/g, ' ').trim();
+			} else if (index === 3) {
+				contest.duration =
+					$(c).text().replace(/\s+/g, ' ').trim() +
+					'(days:hours:minutes)';
+			} else if (index === 4) {
+				contest.status = $(c).text().replace(/\s+/g, ' ').trim();
+			} else if (index === 5) {
+				contest.registerby =
+					$(c).text().replace(/\s+/g, ' ').trim() + '(hh:mm:ss/days)';
+			}
+		});
+		arrayOfContests.push(contest);
 	});
+	// console.log(arrayOfContests);
+	res.json(arrayOfContests);
+});
+
+let a = [],
+	b = [],
+	c = [],
+	d = [],
+	e = [],
+	f = [];
+
+// Request and sort all problems from codeforces into divisions A<B<C<D<E<F
+fetch('https://codeforces.com/api/problemset.problems')
+	.then((data) => data.json())
+	.then((data) => {
+		return data.result.problems;
+	})
+	.then((questions) => {
+		for (let i = 0; i < questions.length; i++) {
+			switch (questions[i].index) {
+				case 'A':
+					a.push(questions[i]);
+					break;
+				case 'B':
+					b.push(questions[i]);
+					break;
+				case 'C':
+					c.push(questions[i]);
+					break;
+				case 'D':
+					d.push(questions[i]);
+					break;
+				case 'E':
+					e.push(questions[i]);
+					break;
+				case 'F':
+					f.push(questions[i]);
+					break;
+			}
+		}
+		console.log(
+			'Finished requesting and grouping questions: ' + questions.length
+		);
+	});
+
+// Get relevant question sets
+app.get('/api/codeforces/problem-set/:index', (req, res) => {
+	let index = req.params.index;
+	switch (index) {
+		case 'a':
+		case 'A':
+			res.json(a);
+			break;
+		case 'b':
+		case 'B':
+			res.json(b);
+			break;
+		case 'c':
+		case 'C':
+			res.json(c);
+			break;
+		case 'd':
+		case 'D':
+			res.json(d);
+			break;
+		case 'e':
+		case 'E':
+			res.json(e);
+			break;
+		case 'f':
+		case 'F':
+			res.json(f);
+			break;
+	}
 });
 
 app.listen(3000, () => {
--- public/index.js
+++ public/index.js
@@ -7,11 +7,15 @@ const codeforcesEle = document.getElementById('codeforces');
 fetch('https://codeforces.com/api/user.rating?handle=' + username)
 	.then((response) => response.json())
 	.then((data) => {
-		const displayHandleEle = document.createElement('h2');
-		const currentRatingEle = document.createElement('h3');
-		const contestDisplay = document.createElement('div');
+		const displayHandleEle = document.getElementById('handle');
+		const currentRatingEle = document.getElementById('current-rating');
+		const contestDisplay = document.getElementById('contests-attended');
 
 		displayHandleEle.textContent = data.result[0].handle;
+		displayHandleEle.setAttribute(
+			'href',
+			'https://codeforces.com/profile/' + username
+		);
 		currentRatingEle.textContent =
 			'Current User Rating: ' +
 			data.result[data.result.length - 1].newRating;
@@ -23,24 +27,80 @@ fetch('https://codeforces.com/api/user.rating?handle=' + username)
 				1 +
 				'. ' +
 				data.result[contest].contestName +
-				'      Rank: ' +
+				' Rank: ' +
 				data.result[contest].rank;
 			contestDisplay.appendChild(pTag);
 		}
-
-		// Add all children
-		codeforcesEle.appendChild(displayHandleEle);
-		codeforcesEle.appendChild(currentRatingEle);
-		const tTag = document.createElement('h3');
-		tTag.textContent = 'Contests Attended';
-		codeforcesEle.append(tTag);
-		codeforcesEle.appendChild(contestDisplay);
 	})
 	.catch((error) => {
 		console.log('Error Occoured: ' + error);
 	});
 
 // Set Upcoming cosntests list with time left in hrs
-fetch(apiBaseUrl + 'codeforces/upcoming-contests').then((data) =>
-	console.log(data)
-);
+fetch(apiBaseUrl + 'codeforces/upcoming-contests')
+	.then((response) => response.json())
+	.then((data) => {
+		console.log(data);
+		const upcomingContestsEle =
+			document.getElementById('upcoming-contests');
+		const tTag = document.createElement('h3');
+		tTag.textContent = 'Upcoming Contests';
+		upcomingContestsEle.appendChild(tTag);
+		let i = 1;
+		for (let c = 0; c < data.length; c++) {
+			let contest = data[c];
+			let newContestDiv = document.createElement('p');
+			newContestDiv.textContent =
+				i++ +
+				'. Name: ' +
+				contest.name +
+				' Date time: ' +
+				contest.dateTime +
+				// ' Duration: ' +
+				// contest.duration +
+				// ' Status: ' +
+				// contest.status +
+				' Registration by/Status: ' +
+				contest.registerby;
+
+			upcomingContestsEle.appendChild(newContestDiv);
+		}
+	});
+
+// Display problems to solve
+// let timeOut = window.setTimeout(() => {
+// 	setQuestions('A');
+// }, 3000);
+// window.clearTimeout(timeOut);
+
+setQuestions('A');
+
+function setQuestions(index) {
+	const questionsDiv = document.getElementById('questions');
+	const qWrap = document.createElement('div');
+	qWrap.setAttribute('id', 'qs');
+
+	// Remove previous questions
+	const pQs = document.getElementById('qs');
+	if (questionsDiv.lastChild === pQs) {
+		questionsDiv.removeChild(pQs);
+	}
+
+	fetch(apiBaseUrl + 'codeforces/problem-set/' + index)
+		.then((response) => response.json())
+		.then((data) => {
+			for (let d = 0; d < data.length; d++) {
+				let newDiv = document.createElement('div');
+				newDiv.innerHTML = `<p>${
+					d + 1
+				}. <a href="https://codeforces.com/contest/${
+					data[d].contestId
+				}/problem/${index}">${data[d].name}</a> (${data[d].points}) - ${
+					data[d].tags
+				} </p>`;
+				qWrap.appendChild(newDiv);
+			}
+			questionsDiv.appendChild(qWrap);
+			console.log('Loaded ' + data.length + '  questions!');
+		});
+}
